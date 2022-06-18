288b251d91aff0833b82f8d56dd08b5cf20d6e61
Serhiy <serhiyhvaladew@gmail.com>
2022-06-18 15:18:03 +0300

    create File & Folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./component/Footer";
 
 const App = () => {
   return (
-    <Box width='400px'>
-        App
-    </Box>);
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
